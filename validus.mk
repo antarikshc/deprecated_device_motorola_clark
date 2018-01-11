7d5d48cba5f1bfcbe21a841284a8e53afc4ed9e4
Antariksh C <antarikshc@gmail.com>
2018-01-11 15:29:10 +0530

    AOSP changes for Validus make file
    some basic changes and added me as a maintainer

--- validus.mk
+++ validus.mk
@@ -4,15 +4,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_clark
+PRODUCT_NAME := validus_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Antariksh C."
+  
