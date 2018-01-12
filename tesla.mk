87eea31c3c0a569d7c9602c99ec7d10d33938ff4
Antariksh C <antarikshc@gmail.com>
2018-01-12 13:33:01 +0530

    Update and rename validus.mk to tesla.mk

--- tesla.mk
+++ tesla.mk
@@ -5,13 +5,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
 # Inherit some common validus stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
-$(call inherit-product, vendor/validus/config/caf_fw.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/caf_fw.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := validus_clark
+PRODUCT_NAME := tesla_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
