00b329ce5cee3c6225dc18d5fdab7b750d2f2f58
Antariksh C <antarikshc@gmail.com>
2018-01-15 18:02:05 +0530

    GZOSP Bringup

--- gzosp.mk
+++ gzosp.mk
@@ -5,12 +5,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_clark
+PRODUCT_NAME := gzosp_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
