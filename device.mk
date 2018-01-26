b9ad59bb1380ea1d42a75489676a8fa0344c02ba
Antariksh C <antarikshc@gmail.com>
2018-01-26 09:16:24 +0000

    build qcom vendor camera HAL and update manifest

--- device.mk
+++ device.mk
@@ -57,6 +57,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
     libcamera \
     libqomx_core \
