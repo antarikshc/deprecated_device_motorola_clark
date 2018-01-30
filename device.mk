bfaa35c1f6c06ac7f84cb140a1420e8715f70d01
Antariksh C <antarikshc@gmail.com>
2018-01-30 18:02:23 +0000

    lets try without HAL3

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.msm8992 \
     libcamera \
